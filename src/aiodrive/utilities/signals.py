b2c50a9b2b6a803472c36fd09a96f69715a02250
Simon Liétar <simon@lietar.net>
2025-07-27 01:14:35 +0200

    Improve handle_signals function

--- src/aiodrive/utilities/signals.py
+++ src/aiodrive/utilities/signals.py
@@ -1,15 +1,33 @@
 import asyncio
 import contextlib
 import signal
-from collections.abc import Collection
+from collections.abc import AsyncIterator, Collection
 
 
 @contextlib.asynccontextmanager
-async def handle_signals(sig_names: Collection[str], /):
+async def handle_signals(sig_names: Collection[str], /) -> AsyncIterator[None]:
+    """
+    Handle specified signals by cancelling the current task.
+
+    If any of the specified signals is received, the current task is cancelled
+    and the signals are no longer listened for. The `asyncio.CancelledError`
+    exception is not propagated to the caller unless the current task has been
+    cancelled again.
+
+    If the call is cancelled, the signals stop being listened for.
+
+    Parameters
+    ----------
+    sig_names
+        The signal names to handle e.g. `('SIGINT',)`.
+    """
+
     loop = asyncio.get_event_loop()
 
     task = asyncio.current_task()
     assert task is not None
+
+    # TODO: Improve
 
     for signame in sig_names:
         loop.add_signal_handler(getattr(signal, signame), task.cancel)
@@ -25,6 +43,15 @@ async def handle_signals(sig_names: Collection[str], /):
 
 
 async def wait_for_signal(sig_names: Collection[str], /):
+    """
+    Wait for any of the specified signals to be received.
+
+    Parameters
+    ----------
+    sig_names
+        The signal names to wait for e.g. `('SIGINT',)`.
+    """
+
     loop = asyncio.get_event_loop()
     future = asyncio.Future()
 

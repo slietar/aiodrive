0b3870d7da41a3850742c0fd13f7a0aea5e5bb6e
Simon Liétar <simon@lietar.net>
2025-09-06 01:06:12 +0200

    Improve run_in_thread_loop function

--- src/aiodrive/utilities/background1.py
+++ src/aiodrive/utilities/background1.py
@@ -2,7 +2,9 @@ import asyncio
 import threading
 from asyncio import Future
 from collections.abc import Awaitable
-from typing import Optional
+from typing import Literal, Optional
+
+from .future_state import FutureState
 
 from .button1 import ThreadSafeButton
 from .versatile import contextualize
@@ -15,52 +17,53 @@ async def run_in_thread_loop[T](target: Awaitable[T], /) -> T:
     origin_task = asyncio.current_task()
     assert origin_task is not None
 
-    thread_exception: Optional[Exception] = None
-    thread_result: Optional[T] = None
+    thread_state: Optional[FutureState[T]] = None
+    thread_task: Optional[asyncio.Task[object]] = None
 
-    thread_task: Optional[asyncio.Task] = None
-    button = ThreadSafeButton()
+    stage: Literal["join", "preparing", "running", "terminating"] = "preparing"
+    stage_change = ThreadSafeButton()
 
     def thread_main():
         thread_loop.run_until_complete(thread_main_async())
-        button.press()
 
     async def thread_main_async():
-        nonlocal thread_exception, thread_result, thread_task
+        nonlocal stage, thread_state, thread_task
 
         thread_task = asyncio.current_task()
-        button.press()
 
-        try:
-            thread_result = await target
-        except asyncio.CancelledError:
-            pass
-        except Exception as e:
-            origin_loop.call_soon_threadsafe(origin_task.cancel)
-            thread_exception = e
+        stage = "running"
+        stage_change.press()
+
+        thread_state = await FutureState.absorb_awaitable(target)
+
+        stage = "join"
+        stage_change.press()
 
     thread = threading.Thread(target=thread_main)
     thread.start()
 
-    await button
+    await stage_change
+    assert stage == "running"
 
     try:
-        await Future()
+        await stage_change
     finally:
-        assert thread_task is not None
+        if stage_change == "running":
+            try:
+                thread_loop.call_soon_threadsafe(thread_task.cancel)
+            except RuntimeError:
+                # Handle a very unlikely race condition where the thread has just exited
+                pass
 
-        if thread_loop.is_running():
-            thread_loop.call_soon_threadsafe(thread_task.cancel)
-            # Problem: the loop could have been stopped running
-            # Problem: the button could have been pressed already
-            await button
+            await stage_change
 
+        assert stage == "join"
         thread.join()
 
-        if thread_exception is not None:
-            raise thread_exception from None
+        assert thread_state is not None
+        return thread_state.apply()
 
-        return thread_result
+        # TODO: Maybe wrap error in exception group for readability
 
 
 async def main():
@@ -69,22 +72,25 @@ async def main():
 
         try:
             await asyncio.sleep(delay)
+            raise ValueError("An error occurred during sleep")
         except asyncio.CancelledError:
             print(f"Sleep for {delay} seconds was cancelled")
             raise
         else:
             print(f"Finished sleeping for {delay} seconds")
 
-    try:
-        async with contextualize(run_in_thread_loop(sleep(1))):
-            await asyncio.sleep(1.5)
-            # Or
-            # await asyncio.sleep(.5)
-            print("Closing")
-    except Exception as e:
-        raise Exception("An error occurred") from e
+    await run_in_thread_loop(sleep(2))
 
-    print("Closed")
+    # try:
+    #     async with contextualize(run_in_thread_loop(sleep(1))):
+    #         await asyncio.sleep(1.5)
+    #         # Or
+    #         # await asyncio.sleep(.5)
+    #         print("Closing")
+    # except Exception as e:
+    #     raise Exception("An error occurred") from e
+
+    # print("Closed")
 
 
 if __name__ == "__main__":

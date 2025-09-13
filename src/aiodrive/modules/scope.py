5bb74e0b79fbe1af677f8ee7729da075d3d502d9
Simon Liétar <simon@lietar.net>
2025-09-14 01:45:57 +0200

    Improve ScopeToken class

--- src/aiodrive/modules/scope.py
+++ src/aiodrive/modules/scope.py
@@ -2,6 +2,7 @@ import asyncio
 import contextlib
 from asyncio import Task
 from dataclasses import dataclass, field
+from typing import Optional
 
 
 @dataclass(slots=True)
@@ -21,7 +22,7 @@ class Scope:
 
 
 @contextlib.asynccontextmanager
-async def use_scope():
+async def use_scope_old():
   """
   Create a context that locally manages the cancellation of the current task.
   """
@@ -40,6 +41,58 @@ async def use_scope():
     if task.cancelling() != 0:
       raise
 
+
+@dataclass(slots=True)
+class ScopeToken:
+  _cancellation_count: int = field(default=0, init=False)
+  _exited: bool = field(default=False, init=False)
+  _task: Task = field(repr=False)
+
+  def cancel(self):
+    if self._exited:
+      raise RuntimeError("Cannot cancel scope after it has exited")
+
+    self._task.cancel()
+    self._cancellation_count += 1
+
+@dataclass(slots=True)
+class use_scope_new:
+  """
+  Create a context that locally manages the cancellation of the current task.
+  """
+
+  _token: Optional[ScopeToken] = field(default=None, init=False, repr=False)
+
+  async def __aenter__(self):
+    assert self._token is None
+
+    current_task = asyncio.current_task()
+    assert current_task is not None
+
+    self._token = ScopeToken(current_task)
+    return self._token
+
+  async def __aexit__(self, exc_type, exc_value, traceback):  # noqa: ANN001
+    assert self._token is not None
+    self._token._exited = True
+
+    try:
+      if (exc_type is None) or not issubclass(exc_type, asyncio.CancelledError):
+        return False
+
+      for _ in range(self._token._cancellation_count):
+        self._token._task.uncancel()
+
+      if self._token._task.cancelling() != 0:
+        return False
+
+      return True
+    finally:
+      self._token = None
+
+
+use_scope = use_scope_new
+# use_scope = use_scope_old
 
 __all__ = [
   'Scope',

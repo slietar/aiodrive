bf2a0a69573cbbe919530c77db68913f80ba9a66
Simon Liétar <simon@lietar.net>
2025-09-14 19:27:20 +0200

    Improve eager_task_group function

--- src/aiodrive/modules/task_group.py
+++ src/aiodrive/modules/task_group.py
@@ -2,24 +2,24 @@ import contextlib
 from asyncio import TaskGroup
 from collections.abc import AsyncIterator
 
+from .cancel import suppress
+
 
 class TaskGroupTerminatedException(Exception):
   pass
 
 @contextlib.asynccontextmanager
-async def use_eager_task_group() -> AsyncIterator[TaskGroup]:
+async def eager_task_group() -> AsyncIterator[TaskGroup]:
   """
   Create a `TaskGroup` that automatically terminates when the context is exited.
   """
 
-  try:
+  with suppress(TaskGroupTerminatedException):
     async with TaskGroup() as group:
       yield group
       raise TaskGroupTerminatedException
-  except* TaskGroupTerminatedException:
-    pass
 
 
 __all__ = [
-  'use_eager_task_group',
+  'eager_task_group',
 ]

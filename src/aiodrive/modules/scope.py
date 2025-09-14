d8ecbb3fd8192dac5ab2e0c89ec02f810ac72679
Simon Liétar <simon@lietar.net>
2025-09-14 19:25:46 +0200

    Improve use_scope function

--- src/aiodrive/modules/scope.py
+++ src/aiodrive/modules/scope.py
@@ -19,7 +19,7 @@ class Scope:
     Raises
     ------
     RuntimeError
-      If the scope has been exited.
+      If the scope has already been exited.
     """
 
     if self.task is None:
@@ -29,8 +29,8 @@ class Scope:
     self.cancellation_count += 1
 
 
-@contextlib.asynccontextmanager
-async def use_scope_old():
+@contextlib.contextmanager
+def use_scope():
   """
   Create a context that locally manages the cancellation of the current task.
   """
@@ -51,58 +51,6 @@ async def use_scope_old():
   finally:
     scope.task = None
 
-
-@dataclass(slots=True)
-class ScopeToken:
-  _cancellation_count: int = field(default=0, init=False)
-  _exited: bool = field(default=False, init=False)
-  _task: Task = field(repr=False)
-
-  def cancel(self):
-    if self._exited:
-      raise RuntimeError("Cannot cancel scope after it has exited")
-
-    self._task.cancel()
-    self._cancellation_count += 1
-
-@dataclass(slots=True)
-class use_scope_new:
-  """
-  Create a context that locally manages the cancellation of the current task.
-  """
-
-  _token: Optional[ScopeToken] = field(default=None, init=False, repr=False)
-
-  async def __aenter__(self):
-    assert self._token is None
-
-    current_task = asyncio.current_task()
-    assert current_task is not None
-
-    self._token = ScopeToken(current_task)
-    return self._token
-
-  async def __aexit__(self, exc_type, exc_value, traceback):  # noqa: ANN001
-    assert self._token is not None
-    self._token._exited = True
-
-    try:
-      if (exc_type is None) or not issubclass(exc_type, asyncio.CancelledError):
-        return False
-
-      for _ in range(self._token._cancellation_count):
-        self._token._task.uncancel()
-
-      if self._token._task.cancelling() != 0:
-        return False
-
-      return True
-    finally:
-      self._token = None
-
-
-# use_scope = use_scope_new
-use_scope = use_scope_old
 
 __all__ = [
   'Scope',

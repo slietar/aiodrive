86c67d2b8b8d5cbecf5e74b49697b5bd56f40c65
Simon Liétar <simon@lietar.net>
2025-11-13 00:22:26 +0100

    Add get_event_loop function

--- src/aiodrive/modules/event_loop.py
+++ src/aiodrive/modules/event_loop.py
@@ -3,6 +3,22 @@ import contextlib
 from asyncio import AbstractEventLoop
 
 
+def get_event_loop():
+  """
+  Get the current event loop.
+
+  Returns
+  -------
+  Optional[AbstractEventLoop]
+    The current event loop, or `None` if there is none.
+  """
+
+  try:
+    return asyncio.get_running_loop()
+  except RuntimeError:
+    return None
+
+
 @contextlib.contextmanager
 def set_event_loop(loop: AbstractEventLoop, /):
   """
@@ -30,5 +46,6 @@ def set_event_loop(loop: AbstractEventLoop, /):
 
 
 __all__ = [
+  'get_event_loop',
   'set_event_loop',
 ]

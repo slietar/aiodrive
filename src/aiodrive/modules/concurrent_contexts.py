77ca04f1d38d12f90d8bcfde176bb0d99e967508
Simon Liétar <simon@lietar.net>
2025-11-13 20:05:45 +0100

    Improve concurrent_contexts function

--- src/aiodrive/modules/concurrent_contexts.py
+++ src/aiodrive/modules/concurrent_contexts.py
@@ -3,24 +3,40 @@ from collections.abc import AsyncIterator, Iterable
 from contextlib import AbstractAsyncContextManager
 from typing import overload
 
-from .shield import ShieldContext
-from .wait import try_all, wait_all
+from .cleanup import cleaned_up
+from .wait import wait_all
 
 
 @overload
-def concurrent_contexts(managers: tuple[()], /) -> AbstractAsyncContextManager[tuple[()]]:
+def concurrent_contexts() -> AbstractAsyncContextManager[tuple[()]]:
     ...
 
 @overload
-def concurrent_contexts[T1](managers: tuple[AbstractAsyncContextManager[T1]], /) -> AbstractAsyncContextManager[tuple[T1]]:
+def concurrent_contexts[T1](
+    manager1: AbstractAsyncContextManager[T1],
+    /,
+) -> AbstractAsyncContextManager[tuple[T1]]:
     ...
 
 @overload
-def concurrent_contexts[T1, T2](managers: tuple[AbstractAsyncContextManager[T1], AbstractAsyncContextManager[T2]], /) -> AbstractAsyncContextManager[tuple[T1, T2]]:
+def concurrent_contexts[T1, T2](
+    manager1: AbstractAsyncContextManager[T1],
+    manager2: AbstractAsyncContextManager[T2],
+    /,
+) -> AbstractAsyncContextManager[tuple[T1, T2]]:
     ...
 
 @overload
-def concurrent_contexts[T1, T2, T3](managers: tuple[AbstractAsyncContextManager[T1], AbstractAsyncContextManager[T2], AbstractAsyncContextManager[T3]], /) -> AbstractAsyncContextManager[tuple[T1, T2, T3]]:
+def concurrent_contexts[T1, T2, T3](
+    manager1: AbstractAsyncContextManager[T1],
+    manager2: AbstractAsyncContextManager[T2],
+    manager3: AbstractAsyncContextManager[T3],
+    /,
+) -> AbstractAsyncContextManager[tuple[T1, T2, T3]]:
+    ...
+
+@overload
+def concurrent_contexts[T](*managers: AbstractAsyncContextManager[T]) -> AbstractAsyncContextManager[tuple[T, ...]]:
     ...
 
 @overload
@@ -28,7 +44,7 @@ def concurrent_contexts[T](managers: Iterable[AbstractAsyncContextManager[T]], /
     ...
 
 @contextlib.asynccontextmanager
-async def concurrent_contexts(managers: Iterable[AbstractAsyncContextManager], /) -> AsyncIterator[tuple]:
+async def concurrent_contexts(*managers: AbstractAsyncContextManager | Iterable[AbstractAsyncContextManager]) -> AsyncIterator[tuple]:
     """
     Enter multiple asynchronous context managers concurrently.
 
@@ -38,6 +54,15 @@ async def concurrent_contexts(managers: Iterable[AbstractAsyncContextManager], /
     cancelled and the entered ones are exited. If any context manager fails to
     exit, the context manager waits for all entered context managers to exit
     without cancelling pending ones.
+
+    Because all context managers are entered and exited concurrently, they are
+    not provided with the potential exception of their context. Context managers
+    that depend on the exception usually provided in `__exit__()`, such as that
+    returned by `contextlib.suppress()`, do not work as expected. Similarly,
+    context managers that update a global state, such as
+    `contextvars.ContextVar.set` which returns a `contextvars.Token`, may not
+    work as expected if the state is read or written to by another context being
+    entered or exited concurrently.
 
     Parameters
     ----------
@@ -51,6 +76,12 @@ async def concurrent_contexts(managers: Iterable[AbstractAsyncContextManager], /
         entering each context manager. The order is maintained.
     """
 
+    if managers and isinstance(managers[0], Iterable):
+        assert len(managers) == 1
+        effective_managers = tuple(managers[0])
+    else:
+        effective_managers: tuple[AbstractAsyncContextManager, ...] = managers # type: ignore
+
     # Some managers may fail to enter, so we must track which ones succeeded.
     open_managers = list[AbstractAsyncContextManager]()
 
@@ -59,12 +90,11 @@ async def concurrent_contexts(managers: Iterable[AbstractAsyncContextManager], /
         open_managers.append(manager)
         return context_value
 
-    context = ShieldContext()
+    async def exit_contexts():
+        await wait_all(manager.__aexit__(None, None, None) for manager in open_managers)
 
-    try:
-        yield tuple(await try_all(enter_context(manager) for manager in managers))
-    finally:
-        await context.shield(wait_all(manager.__aexit__(None, None, None) for manager in open_managers))
+    async with cleaned_up(exit_contexts):
+        yield tuple(await wait_all(enter_context(manager) for manager in effective_managers))
 
 
 __all__ = [

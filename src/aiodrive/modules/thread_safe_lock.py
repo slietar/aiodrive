d7aa0a8c21401724136b550acd0bc805888e0cd2
Simon Liétar <simon@lietar.net>
2026-01-11 22:48:58 +0100

    Add synchronous interface to ThreadsafeLock

--- src/aiodrive/modules/thread_safe_lock.py
+++ src/aiodrive/modules/thread_safe_lock.py
@@ -2,43 +2,64 @@ import asyncio
 from asyncio import Future
 from collections import deque
 from dataclasses import dataclass, field
+from threading import Condition as ThreadingCondition
 from threading import Lock as ThreadingLock
 
 
 @dataclass(slots=True)
 class ThreadsafeLock:
-  _futures: deque[Future[None]] = field(default_factory=deque, init=False, repr=False)
   _locked: bool = field(default=False, init=False, repr=False)
+  _waiters: deque[Future[None]] = field(default_factory=deque, init=False, repr=False)
+
+  # _waiters_lock: ThreadingCondition = field(default_factory=ThreadingCondition, init=False, repr=False)
+  _state_lock: ThreadingLock = field(default_factory=ThreadingLock, init=False, repr=False)
   _sync_lock: ThreadingLock = field(default_factory=ThreadingLock, init=False, repr=False)
 
+
   def __enter__(self):
-    asyncio.run(self.acquire())
+    self.acquire_sync()
 
   def __exit__(self, exc_type, exc_value, traceback):  # noqa: ANN001
-    self.release()
+    self.release_sync()
+
+  def acquire_sync(self):
+    while True:
+      self._sync_lock.acquire()
+
+      with self._state_lock:
+        if not self._locked:
+          self._locked = True
+          break
+
+        self._sync_lock.release()
+
+  def release_sync(self):
+    self.release_async()
+
 
   async def __aenter__(self):
-    await self.acquire()
+    await self.acquire_async()
 
   async def __aexit__(self, exc_type, exc_value, traceback):  # noqa: ANN001
-    self.release()
+    self.release_async()
 
-  async def acquire(self):
-    with self._sync_lock:
+  async def acquire_async(self):
+    with self._state_lock:
       if not self._locked:
+        self._sync_lock.acquire()
         self._locked = True
         return
 
       future = Future[None]()
-      self._futures.append(future)
+      self._waiters.append(future)
 
     # We purposefully let the future potentially be cancelled
     await future
 
-  def release(self):
-    with self._sync_lock:
-      while self._futures:
-        future = self._futures.popleft()
+  def release_async(self):
+    with self._state_lock:
+      while self._waiters:
+        future = self._waiters.popleft()
 
         if future.cancelled():
           continue
@@ -51,6 +72,7 @@ class ThreadsafeLock:
         break
       else:
         self._locked = False
+        self._sync_lock.release()
 
 
 @dataclass(slots=True)
@@ -69,7 +91,7 @@ class ThreadsafeCondition:
     self._waiters.clear()
 
   async def wait(self):
-    self.lock.release()
+    self.lock.release_async()
 
     future = Future[None]()
     self._waiters.add(future)
@@ -77,13 +99,13 @@ class ThreadsafeCondition:
     try:
       await future
     finally:
-      await self.lock.acquire()
+      await self.lock.acquire_async()
 
   async def __aenter__(self):
-    await self.lock.acquire()
+    await self.lock.acquire_async()
 
   async def __aexit__(self, exc_type, exc_val, exc_tb):  # noqa: ANN001
-    self.lock.release()
+    self.lock.release_async()
 
 
 __all__ = [

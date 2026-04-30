88f75ca480b71bea34fd1abf5e129a367a9b4ccb
Simon Liétar <simon@lietar.net>
2026-05-01 00:41:44 +0200

    Improve ThreadManager class

--- src/aiodrive/modules/thread_manager.py
+++ src/aiodrive/modules/thread_manager.py
@@ -1,9 +1,12 @@
 import asyncio
-from asyncio import Future, TaskGroup
+from asyncio import Future, Task, TaskGroup
 from collections.abc import Awaitable
 
 from .awaitable import wait_forever
+from .cancel import suppress
 from .future_state import FutureState
+from .guaranteed_task import GuaranteedTask
+from .shield import shield
 from .thread_loop import run_in_thread_loop_contextualized
 
 
@@ -11,12 +14,33 @@ class ThreadManager:
     """
     A class for managing the execution of awaitables in a separate thread that
     has its own event loop.
+
+    When exiting, the thread manager waits for all tasks to finish.
     """
 
-    def _create[T](self, awaitable: Awaitable[T], future: Future[T]):
-        async def create_task():
-            (await FutureState.absorb_awaitable(awaitable)).transfer_threadsafe(future)
+    def __init__(self):
+        self._next_task_id = 0
+        self._tasks = dict[int, Task]()
 
+
+    def _cancel(self, task_id: int):
+        task = self._tasks.get(task_id)
+
+        if task is not None:
+            task.cancel()
+
+    def _create[T](self, task_id: int, awaitable: Awaitable[T], future: Future[T]):
+        async def create_task():
+            try:
+                (await FutureState.absorb_awaitable(inner_task)).transfer_threadsafe(future)
+            finally:
+                del self._tasks[task_id]
+
+        async def coro():
+            return await awaitable
+
+        inner_task = GuaranteedTask(coro())
+        self._tasks[task_id] = inner_task
         self._group.create_task(create_task())
 
     async def _main(self):
@@ -59,16 +83,36 @@ class ThreadManager:
             The result of the provided awaitable.
         """
 
+        task_id = self._next_task_id
+        self._next_task_id += 1
+
         future = Future[T]()
 
         self._thread_loop.call_soon_threadsafe(
             self._create,
+            task_id,
             awaitable,
             future,
         )
 
-        return await future
+        cancelled = False
 
+        while True:
+            try:
+                result = await shield(future)
+            except asyncio.CancelledError:
+                if future.done():
+                    raise
+
+                with suppress(RuntimeError):
+                    self._thread_loop.call_soon_threadsafe(self._cancel, task_id)
+            else:
+                break
+
+        if cancelled:
+            raise asyncio.CancelledError
+
+        return result
 
     def __enter__(self):
         self._context_manager = run_in_thread_loop_contextualized(self._main())
